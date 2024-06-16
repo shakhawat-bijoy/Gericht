79ed6e2020f1edc1f537ff7f1d1230b0d35b1e9e
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-06-17 00:18:59 +0600

    Banner Slider

--- src/componensts/Banner.jsx
+++ src/componensts/Banner.jsx
@@ -20,8 +20,9 @@ const Banner = () => {
     arrows: false,
     infinite: true,
     fade: true,
+    autoplay: true,
     speed: 2000,
-    autoplaySpeed: 6000,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
 
